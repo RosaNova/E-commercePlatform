4173842f5fffa27b232dbddbf8c40c7c2007b74f
r_sunrise <rozasun61@gmail.com>
2026-02-12 16:38:00 +0700

    🧪test(logic): addtocart store in localStorage and get into Shoppingcart

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
+
 const openShoppingBtn = document.getElementById("openShoppingBtn");
 const shoppingContainer = document.getElementById("shoppingContainer");
 const closeShoppingBtn = document.getElementById("closeShoppingBtn");
 const shoppingBody = document.getElementById("shoppingBody");
 const allProducts = document.getElementById("allProducts");
+const btnShopNow = document.getElementById("shopNowBtn");
 
 
 openShoppingBtn.addEventListener("click", () => {
@@ -16,9 +18,7 @@ closeShoppingBtn.addEventListener("click", () => {
 });
 
 
-
 let products = [];
-
 async function fetchProducts() {
     try {
         const response = await fetch('/src/data/product.json');
@@ -47,8 +47,6 @@ function renderProducts() {
         const productDiv = document.createElement("div");
         productDiv.className = "product-card shadow-lg relative rounded-xl p-4";
 
-
-
         productDiv.innerHTML = `
             <button
                 class="favorithBtn absolute top-5 right-5 bg-secondary rounded-full p-2 cursor-pointer text-background text-3xl "
@@ -63,16 +61,57 @@ function renderProducts() {
                 <h2 class="text-2xl font-bold text-secondary">${product.name}</h2>
                 <p class="text-xl pb-3 font-bold text-secondary">$${product.price.toFixed(2)}</p>
                 <button
-                    class="bg-secondary w-full rounded-xl p-2 cursor-pointer font-bold text-background text-xl ">Shop
+                    onclick="addTocart(${product.id})"
+                    class="bg-secondary  w-full rounded-xl p-2 cursor-pointer font-bold text-background text-xl ">Shop
                     Now</button>
             </div>
         `;
         allProducts.appendChild(productDiv);
     });
 
-    // Re-initialize Lucide icons for newly added heart icons
     if (window.lucide) {
         window.lucide.createIcons();
     }
 }
+
 fetchProducts();
+
+let AllShoppingCart = JSON.parse(localStorage.getItem("ShoppingCart")) || [];
+
+function addTocart(id) {
+    const product = products.find(product => product.id === id);
+    AllShoppingCart.push(product);
+    localStorage.setItem("ShoppingCart", JSON.stringify(AllShoppingCart));
+}
+
+function renderShoppingCart() {
+    let result = "";
+    AllShoppingCart.forEach(product => {
+        result += `
+  <div
+                    class="cart h-[100px] relative flex items-center justify-between gap-2 text-secondary bg-background rounded-2xl p-2">
+                    <img class="h-full object-cover object-top w-[100px] rounded-xl"
+                        src="${product.image}" alt="">
+                    <div class="info">
+                        <h2 class="text-xl font-bold ">${product.name}</h2>
+                        <p class="text-xl font-bold">$${product.price.toFixed(2)}</p>
+                    </div>
+                    <div class="quantity flex mr-20 items-center gap-2">
+                        <button
+                            class="bg-secondary rounded-xl px-3.5 cursor-pointer text-background text-3xl ">-</button>
+                        <span>${product.quantity}</span>
+                        <button
+                            class="bg-secondary rounded-xl px-2.5 cursor-pointer text-background text-3xl ">+</button>
+                    </div>
+                    <button id="cancelBtn"
+                        class="close cursor-pointer absolute top-3 right-3 bg-secondary text-black rounded-[5px] p-1"><i
+                            data-lucide="x"></i></button>
+                </div>
+           `
+        shoppingBody.innerHTML += result;
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    renderShoppingCart();
+});
